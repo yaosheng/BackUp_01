575840c0aea88de6153c4c6a5e1418f5f606915f
yaoshengchuang <yaosheng22@gmail.com>
2024-10-06 22:24:33 +0800

    1.0 正式版

--- Program.cs
+++ Program.cs
@@ -1,20 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
 using System.IO;
+using System.Net.Http.Headers;
 using BackUp_01;
 
 try
 {
-    Console.WriteLine("Hello, World!");
+    List<string> directoryList1 = [];
+    List<string> directoryList2 = [];
+    string disk1 = "";
+    string disk2 = "";
 
     DriveInfo[] drives = DriveInfo.GetDrives();
-
-    Console.WriteLine(drives);
-
-    DriveInfo driveBACKUP_1;
-    DriveInfo driveBACKUP_2;
-    Dictionary<string, DirectoryInfo> dir1 = [];
-    Dictionary<string, DirectoryInfo> dir2 = [];
 
     foreach (var item in drives.ToList())
     {
@@ -22,19 +19,66 @@ try
         {
             if (item.VolumeLabel == "BACKUP_1")
             {
-                driveBACKUP_1 = item;
+                disk1 = item.Name.Substring(0, 1);
+                directoryList1 = Tools.GetALlDirectories(item);
             }
             if (item.VolumeLabel == "BACKUP_2")
             {
-                driveBACKUP_2 = item;
+                disk2 = item.Name.Substring(0, 1);
             }
         }
     }
 
+    #region 處理資料夾
+    //將J換成K
+    foreach(var item in directoryList1)
+    {
+        directoryList2.Add(disk2 + item.Substring(1, item.Length - 1));
+    }
+
+    foreach(var item in directoryList2)
+    {
+        if (!Directory.Exists(item))
+        {
+            Directory.CreateDirectory(item);
+        }
+    }
+
+    #endregion
+
+    #region 處理檔案
+       
+    List<string> dataPaths = new List<string>();
+
+    foreach (var item in directoryList1)
+    {
+        var tmp = Directory.GetFiles(item);
+        foreach(var filePath in tmp)
+        {
+            dataPaths.Add(disk2 + filePath.Substring(1, filePath.Length - 1));
+        }
+    }
+
+    foreach (var item in dataPaths)
+    {
+        try
+        {
+            if (!File.Exists(item))
+            {
+                string sourcePath = disk1 + item.Substring(1, item.Length - 1);
+                File.Copy(sourcePath, item);
+            }
+        }
+        catch(Exception e)
+        {
+            continue;
+        }
+    }
 
 
+    Console.WriteLine(dataPaths);
+    #endregion
 
-    Console.WriteLine(  );
 }
 catch(Exception e)
 {
--- Tools.cs
+++ Tools.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,48 @@ namespace BackUp_01
 {
     internal class Tools
     {
+        public static List<string> GetALlDirectories(DriveInfo info)
+        {
+            List<string> finalDirects = new List<string>();
+            List<string> directories = Directory.GetDirectories(info.Name).ToList();
+            List<string> newDirectories = new List<string>();
 
+            foreach (var item in directories)
+            {
+                if (!item.Contains("$RECYCLE.BIN") && !item.Contains("System"))
+                {
+                    newDirectories.Add(item);
+                    finalDirects.Add(item);
+                }
+            }
+            //找出所有資料夾(遞迴)
+            GetDirectoryInfos(ref finalDirects, newDirectories.ToArray());
+
+            return finalDirects;
+        }
+
+        private static void GetDirectoryInfos(ref List<string> dicts, string[] paths)
+        {
+            List<DirectoryInfo> directoryInfos = new List<DirectoryInfo>();
+
+            foreach (var path in paths)
+            {
+                List<string> directories = Directory.GetDirectories(path).ToList();
+                if(directories.Count != 0)
+                {
+                    foreach (var d in directories) {
+                        if (Directory.Exists(d)) {
+                            dicts.Add(d);
+                        }
+                    }
+                    GetDirectoryInfos(ref dicts, directories.ToArray());
+                }
+                else
+                {
+
+                }
+                Console.WriteLine();
+            }
+        }
     }
 }
